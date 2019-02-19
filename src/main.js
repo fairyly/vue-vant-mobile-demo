de98b7bb86b220cacfa1634dc2f6b5ba0c090a8b
fairyly <fairyilys@gmail.com>
2019-02-19 23:21:13 +0800

    feat: 增加 ployfill

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import 'amfe-flexible';
 import Vue from 'vue';
 import store from './store/index'
 import App from './App';
-import {
-  router
-} from './router';
+import { router } from './router';
+import 'babel-polyfill';
+import Es6Promise from 'es6-promise';
+require('es6-promise').polyfill();
+Es6Promise.polyfill();
 import './assets/font/iconfont.css';
 import './assets/css/common.css';
 
